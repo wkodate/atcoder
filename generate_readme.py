c69b703439a008c6a570d7e677469a834d530f36
wkodate <hbwandeow@gmail.com>
2022-02-10 23:24:16 +0900

    Update README.md by generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -1,8 +1,7 @@
 import glob
-import re
 
 java_links = {}
-contest_problem_map = set()
+contest_problems_map = {}
 
 def gather_java_file_path():
     java_file_root_path ="./src/main/java/com/wkodate/atcoder"
@@ -12,17 +11,26 @@ def gather_java_file_path():
         if contest_title.startswith("_"):
             continue
         problem = file.split("/")[8]
+        pset = set()
+        if contest_title in contest_problems_map:
+            pset = contest_problems_map.get(contest_title)
+            pset.add(problem)
+        else:
+            pset = set([problem])
+        contest_problems_map[contest_title] = pset
         contest_problem = "{}/{}".format(contest_title, problem)
-        contest_problem_map.add(contest_problem)
         java_links[contest_problem] = "{}/Main.java".format(file)
 
-def update_readme(java_links):
-    s = "AtCoder\n===\n\n| a | b | c | d | e | f |\n| -- | -- | -- | -- | -- | -- |\n"
-    for k, v in sorted:
-        java_filepath = ""
-        if k in java_links:
-            java_filepath = "[solution]({})".format(java_links.get(k))
-        s += "| {} | {} | {} | {} | {} | {} | \n".format(str(k), v, java_filepath)
+def update_readme(java_links, contest_plist_map):
+    s = "AtCoder\n===\n\n ## Java\n| | A | B | C | D | E | F | G |\n| -- | -- | -- | -- | -- | -- | -- | -- |\n"
+    for c, pset in contest_plist_map:
+        s += "| {} ".format(c)
+        for p in ["a", "b", "c", "d", "e", "f", "g"]:
+            java_filepath = ""
+            if p in pset:
+                java_filepath = "[solution]({})".format(java_links["{}/{}".format(c, p)])
+            s += "| {} ".format(java_filepath)
+        s += "| \n"
     
     with open('./README.md', mode='w') as f:
         f.write(s)
@@ -30,5 +38,5 @@ def update_readme(java_links):
 
 
 gather_java_file_path()
-sorted = sorted(contest_set.items(), key=lambda x:x[0])
-update_readme(java_links)
+sorted_contest_problem_list_map = sorted(contest_problems_map.items(), key=lambda x:x[0])
+update_readme(java_links, sorted_contest_problem_list_map)
