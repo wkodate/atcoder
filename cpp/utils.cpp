5b04d4ebe7c95943409996750a8fca769fa99130
wkodate <hbwandeow@gmail.com>
2022-04-04 21:54:36 +0900

    add next permutation in utils.cpp

--- cpp/utils.cpp
+++ cpp/utils.cpp
@@ -134,6 +134,38 @@ struct Utils {
     }
     return right;
   }
+
+  void swap(vector<int> &nums, int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+
+  void reverse(vector<int> &nums, int start) {
+    int i = start;
+    int j = nums.size() - 1;
+    while (i < j) {
+      swap(nums, i, j);
+      i++;
+      j--;
+    }
+  }
+
+  void next_permutation(vector<int> &nums) {
+    int i = nums.size() - 2;
+    while (i >= 0 && nums[i + 1] <= nums[i]) {
+      i--;
+    }
+    if (i >= 0) {
+      int j = nums.size() - 1;
+      while (nums[j] <= nums[i]) {
+        j--;
+      }
+      swap(nums, i, j);
+    }
+    reverse(nums, i + 1);
+  }
+
 };
 
 int main() {}
